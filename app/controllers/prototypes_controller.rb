081fa2135c045ad2b04fa1d1b41ec52dc7be3baa
sourcecode-bear <76414122+sourcecode-bear@users.noreply.github.com>
2021-01-21 16:00:13 +0900

    アクセス制限の修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,11 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user!, only:[:create, :edit, :destroy]
+  #ログインしていないと遷移できないようにページ制限をする
+  before_action :authenticate_user!, only:[:new, :create, :update, :destroy]
+  #他のログインユーザーがURLを使って遷移しない仕様にする
+  #編集ページに遷移したらトップページに戻る仕様にする
+  before_action :move_to_edit, except:[:index, :show]
+  
+  
   def index
     @prototypes = Prototype.all
   end
@@ -25,7 +31,7 @@ class PrototypesController < ApplicationController
 
   def edit
      @prototype = Prototype.find(params[:id])
-    unless user_signed_in?
+     unless user_signed_in?
       redirect_to action: :index 
     end
   end
@@ -51,5 +57,17 @@ class PrototypesController < ApplicationController
  def prototype_params
   params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
  end
+
+    #条件：プロトタイプを投稿したユーザーが本来のログインしたユーザーかどうか？
+  def move_to_edit
+    unless user_signed_in?
+      prototype.user == current_user
+    else
+      redirect_to action: :index
+    end
+  end
 end
+
+
+
 
